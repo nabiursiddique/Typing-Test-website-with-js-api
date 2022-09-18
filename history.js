e06d8d1845da282f7b517b95ea42f9acf626b7d8
nabiursiddique <nabiursiddique01@gmail.com>
2022-09-18 14:04:08 +0530

    I have added this file after completing the full website thats why here only this commit is commited

---
+++ history.js
@@ -0,0 +1,42 @@
+const histories = document.getElementById("histories");
+
+function addHistory(questionText, timeTaken, errorCount, wordPerMinute) {
+  const newRow = document.createElement("div");
+  newRow.classList.add("card");
+
+  newRow.innerHTML = `
+  <h3>${questionText}</h3>
+  <div>
+  <p>You took: <span class="bold">${timeTaken}</span> seconds</p>
+  <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
+  <p>Your speed was <span class="bold green">${wordPerMinute}</span> WPM</p>
+  </div>
+  `;
+
+  histories.appendChild(newRow);
+
+  let previousTests = JSON.parse(localStorage.getItem("testHistory")) || [];
+  previousTests.push({ questionText, timeTaken, errorCount, wordPerMinute });
+  localStorage.setItem("testHistory", JSON.stringify(previousTests));
+
+  displayHistory();
+}
+
+function displayHistory() {
+  histories.innerHTML = "";
+  const previousTests = JSON.parse(localStorage.getItem("testHistory")) || [];
+
+  previousTests.forEach((test) => {
+    const newRow = document.createElement("div");
+    newRow.classList.add("card");
+
+    newRow.innerHTML = `
+  <h3>${test.questionText}</h3>
+  <p>You took: <span class="bold">${test.timeTaken}</span> seconds</p>
+    <p>You made <span class="bold red">${test.errorCount}</span> mistakes</p>
+    <p>Your speed was <span class="bold green">${test.wordPerMinute}</span> WPM</p>
+  `;
+
+    histories.appendChild(newRow);
+  });
+}
